21628214f40ec9e14ab9433325f7aa644aa6edb3
khaled al shishani <khaled.shishani32@gmail.com>
2021-06-14 22:57:37 +0300

    lab07-server

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
+
 const express = require('express') // require the express package
 const app = express() // initialize your express app instance
  
+const weatherData = require('./data/weather.json');
+
 const cors = require('cors');
 
 app.use(cors()) // after you initialize your express app instance
@@ -13,6 +16,15 @@ app.get('/', // our endpoint name
  function (req , res) { // callback function of what we should do with our request
   res.send('Hello World') // our endpoint function response
 })
+
+
+// a new endpoint 
+
+app.get('/weather' , (req , res)=>{
+  
+    res.json(weatherData);
+})
+
  
 app.listen(port,()=>{
     console.log(`server running in port: ${port}`)
